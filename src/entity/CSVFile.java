35a625c4e76fd47479f9a289ab3ed223ccc5fc27
Nishant Rawat <nishantrawat22@gmail.com>
2018-03-22 23:14:00 -0700

    Set up CSV normalization process stub and members for CSVFile

--- src/entity/CSVFile.java
+++ src/entity/CSVFile.java
@@ -1,5 +1,32 @@
 package entity;
 
-public class CSVFile {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVFile extends File{
+	
+	private String headerLine;
+	private List<Row> rows = new ArrayList<Row>();
+	
+	public CSVFile(String fileName) {
+		super(fileName);
+	}
+	
+	public String getHeaderLine() {
+		return headerLine;
+	}
+
+	public void setHeaderLine(String headerLine) {
+		this.headerLine = headerLine;
+	}
+
+	public List<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(List< Row> rows) {
+		this.rows = rows;
+	}
 
 }
--- src/manager/CSVNormalizationTool.java
+++ src/manager/CSVNormalizationTool.java
@@ -1,5 +1,28 @@
 package manager;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import entity.CSVFile;
+
 public class CSVNormalizationTool {
 
+	public static void main(String[] args) {
+
+		if (args[0].isEmpty()) {
+			System.err.println("No filename specified");
+			return;
+		}
+
+		CSVFile csvFile = new CSVFile(args[0]);
+		if (csvFile.isDirectory() || !csvFile.exists()) {
+			System.err.println("Invalid file");
+		} else {
+
+			// Populate into CSV data structure and transform
+
+			// Export into CSV file
+		}
+	}
 }
