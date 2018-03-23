2be305260017677f92568b634344b373d45bc667
Nishant Rawat <nishantrawat22@gmail.com>
2018-03-22 23:17:11 -0700

    Create a row class with column name member variables

--- src/entity/Row.java
+++ src/entity/Row.java
@@ -1,5 +1,71 @@
 package entity;
 
 public class Row {
-
+	private String timestamp;
+	private String zipCode;
+	private String name;
+	private String address;
+	private String fooDuration;
+	private String barDuration;
+	private String totalDuration;
+	private String notes;
+	
+	private static final char QUOTE = '"';
+	private static final char SEPARATOR = ',';
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		//TODO
+	}
+	
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		//TODO
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		//TODO
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		//TODO
+	}
+	
+	public String getFooDuration() {
+		return fooDuration;
+	}
+	public void setFooDuration(String fooDuration) {
+		//TODO
+	}
+	
+	public String getBarDuration() {
+		return barDuration;
+	}
+	public void setBarDuration(String barDuration) {
+		//TODO
+	}
+	
+	public String getTotalDuration() {
+		return totalDuration;
+	}
+	public void setTotalDuration(String totalDuration) {
+		//TODO
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		//TODO
+	}
 }
